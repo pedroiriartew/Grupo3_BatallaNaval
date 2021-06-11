21838c2b047fc5244c327c1a613807c7a53fb09a
pedroiriartew <pedroiriartew@yahoo.com.ar>
2021-06-11 10:00:16 -0300

    Actualizado tablero

--- BatallaNaval_Grupo3/src/TableroP.java
+++ BatallaNaval_Grupo3/src/TableroP.java
@@ -10,33 +10,48 @@ public class TableroP{
 	private Character [][] panel= new Character [TAMANIO_X][TAMANIO_Y];
 	private List<Boat> fleet = new ArrayList<>();
 	
-	
-	//constructor- ya inicio el tablero
-	public TableroP() {
-		for(int i=0; i<panel.length;i++) {
-			for(int j=0; j<panel.length; j++) {
+
+	public TableroP()
+	{
+		initializePanel();
+	}
+
+	public void initializePanel()
+	{
+		for(int i=0; i<TAMANIO_X;i++) {
+			for(int j=0; j<TAMANIO_Y; j++) {
 				panel[i][j]= ' ';
 			}
 		}
 	}
 	
-	
 	//c es la coordenada (x,y)
 	private boolean validateCoordinate(Coordinate c) {
-		return (c.getX()>= 1 && c.getX()<= panel.length) && (c.getY()>= 1 && c.getY()<= panel.length);
+		return (c.getX()>= 1 && c.getX()<= TAMANIO_X) && (c.getY()>= 1 && c.getY()<= TAMANIO_Y);
 	}
-	
-	private void disparar (Coordinate c) {
 
+	public void shoot(Coordinate c)
+	{
+		hit(c);
+	}
+
+	
+	private void hit (Coordinate c) {		
 		if(validateCoordinate(c)) {
-			
 			if((panel[c.getX()-1][c.getY()-1]) == ' ')
 			{
-				//TODO generar impactos
+				panel[c.getX()-1][c.getY()-1]= 'A';
 			}
-		 }
+		}
+		else
+		{
+			throw new RuntimeException("La coordenada no está dentro del rango esperado.");
+		}
 			
 	}
+
+
+
 	public String print() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\t");
@@ -51,8 +66,9 @@ public class TableroP{
 				if( j == 0)
 					sb.append((i+1)+")\t ("+panel[i][j]+")\t ");
 				else
-					sb.append("("+ panel[i][j]+")\t");
+					sb.append("(" + panel[i][j]+")\t");
 			}
+			sb.append("\n");
 		}
 		return sb.toString();
 	}
