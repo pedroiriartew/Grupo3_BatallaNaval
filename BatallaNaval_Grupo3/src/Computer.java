cf38c88c56957a18b5495ee374b91d88a78df6ec
Usuario <Usuario@DESKTOP-T6CVFQ5>
2021-06-13 23:23:56 -0300

    clase PC con el generador aleatorio de coordenadas

---
+++ BatallaNaval_Grupo3/src/Computer.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Computer extends User{
+
+	//constructor de la computadora
+		public Computer() {
+			this.name= "PC";
+			mainBoard= new TableroP(); 			//tablero se inicializa, crea la flota y la coloca en tablero
+		
+		}
+		
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public Coordinate makeCoordinate() {
+			Random random= new Random();
+			int posX= random.nextInt(10);
+			int posY= random.nextInt(10);
+			Coordinate c= new Coordinate(posX, posY);
+			return c;
+		}
+		
+		
+}
