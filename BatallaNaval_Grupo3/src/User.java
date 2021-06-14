b3e75a336e823c383c71ea2ed905f0b487758439
Usuario <Usuario@DESKTOP-T6CVFQ5>
2021-06-13 23:31:46 -0300

    clase abstracta de usuario

---
+++ BatallaNaval_Grupo3/src/User.java
@@ -0,0 +1,15 @@
+
+
+public abstract class User {
+	protected String name;
+	protected TableroP mainBoard; 			
+	//el tablero del rival deberia imprimirse desde el tablero principal de mi oponente sin mostrar sus barcos
+	
+	public String getUsername() {
+		return name;
+	}
+	
+	public abstract Coordinate makeCoordinate() {
+		
+	}
+}
